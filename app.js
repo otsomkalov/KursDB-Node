ceacc91264f1b75173786a3bb8ba7092665ac256
Oleg Tsomkalov <infinitu1327@gmail.com>
2020-01-05 14:31:18 +0200

    Remove node_modules

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-let session=require('express-session');
-let pgSession=require('connect-pg-simple');
-let db=require('./db.js');
+let session = require('express-session');
+let pgSession = require('connect-pg-simple');
+let db = require('./db.js');
 let app = express();
-let pg=require('pg');
-let routes=require('./routes/root');
+let pg = require('pg');
+let routes = require('./routes/root');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,29 +19,28 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(require('cookie-parser')());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-process.env.DATABASE_URL="postgres://postgres:123@127.0.0.1:5432/policestation";
+process.env.DATABASE_URL = "postgres://postgres:123@127.0.0.1:5432/policestation";
 
 app.use(session({
     store: new (require('connect-pg-simple')(session))(process.env.DATABASE_URL),
     secret: "Police",
     resave: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 },
-    saveUninitialized:false
+    cookie: {maxAge: 24 * 60 * 60 * 1000},
+    saveUninitialized: false
 }));
 
 routes(app);
 
 // error handler
-app.use((err, req, res, next)=>{
-    if (err==404){
+app.use((err, req, res, next) => {
+    if (err == 404) {
         res.render('./general/404');
-    }
-    else{
+    } else {
         res.render('./general/500');
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
